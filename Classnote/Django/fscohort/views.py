e38cf736748d6d5c39b925f7f65a0058ed3e0ef8
faruk <kursunomerfaruk2@gmail.com>
2022-12-08 11:58:58 +0100

    django

---
+++ Classnote/Django/fscohort/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Hello this is our hpme page ...')
+
+def students(request):
+    return HttpResponse('ali, veli, deli')
