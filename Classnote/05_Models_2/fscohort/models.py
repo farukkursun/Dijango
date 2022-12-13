a57d2e04e91ea1d7e11ab1920835954d1f65937f
faruk <kursunomerfaruk2@gmail.com>
2022-12-13 08:56:10 +0100

    update

--- Classnote/05_Models_2/fscohort/models.py
+++ Classnote/05_Models_2/fscohort/models.py
@@ -18,9 +18,34 @@ class Student(models.Model):
 
 
 # modelin varsayilan özelliklerini degistirmek icin kullanilir
-
+# https://docs.djangoproject.com/en/4.1/ref/models/options/#model-meta-options
+        # db_table = 'ogrenci' # tablo_adı değiştir.
     class Meta:
         ordering=['number']
         verbose_name='ögrenci' #model ismi
         verbose_name_plural='ogrenciler' #modelin cogul ismi
     
+
+
+# ! Bir model eklendiğinde/değiştirildiğinde aşağıdaki komutları unutmuyoruz:
+# $ python manage.py makemigrations
+# $ python manage.py migrate
+
+'''
+    # Django - Shell + ORM
+    # https://docs.djangoproject.com/en/4.1/ref/models/querysets/#field-lookups
+    $ python manage.py shell
+    > from fscohort.models import Student
+    > objname = Student.objects.create(first_name='Abc', last_name='Def', number=5) # Yeni Kayıt
+    > objname = Student.objects.all() # Tüm kayıtları getir (liste)
+    > objname = Student.objects.get(number=5) # Number=5 olan kaydı getir (tek kayıt)
+    > objname = Student.objects.filter(number=5) # Number=5 olan kayıtları getir (liste)
+    > objname = Student.objects.filter(first_name__startswith='Q') # firstname 'Q' ile başlayan kayıtları getir (liste)
+    > objname = Student.objects.filter(last_name__endswith='n') # lastname 'n' ile biten kayıtları getir (liste)
+    > objname = Student.objects.filter(last_name__contains='a') # lastname 'a' içeren kayıtları getir (liste)
+    > objname = Student.objects.exclude(number=5) # Number=5 olMAyan kayıtları getir (liste)
+    > objname = Student.objects.exclude(last_name__contains='a') # lastname 'a' içerMEyen kayıtları getir (liste)
+    > objname = Student(first_name='Abc', last_name='Def', number=5) # DB'den bağımsız instance (obje)
+    > objname.save() # tek kayıt olarak gelen objeyi veritabanına ekler veya günceller.
+
+'''
