4bd6487b38a08003187c0efd39924ef16a4baff9
faruk <kursunomerfaruk2@gmail.com>
2022-12-14 09:54:22 +0100

    views

---
+++ Classnote/07_DRF_FunctionBasedViews/student_api/views.py
@@ -0,0 +1,10 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+
+@api_view()
+def home(request):
+    return Response({
+        'message':'hello word'
+    })
